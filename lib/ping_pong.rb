c683ac999f085322d68674b2ffdf46957b31b3cf
Student <example@example.com>
2019-07-15 11:36:09 -0700

    add specs and pingpong method

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,1 +1,21 @@
 require('pry')
+class Integer
+  def ping_pong()
+    x = 0
+    new_array = []
+    while (x < self)
+      x = x + 1
+      if (x % 3 === 0) & (x % 5 === 0)
+        new_array.push("pingpong")
+      elsif (x % 5 === 0)
+        new_array.push("pong")
+      elsif (x % 3 === 0)
+        new_array.push("ping")
+      else
+        new_array.push(x)
+      end
+    end
+    puts (new_array)
+    new_array
+  end
+end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -1,2 +1,11 @@
 require('rspec')
 require('ping_pong')
+
+describe('#ping_pong') do
+  it("counts up to entered number") do
+    expect((5).ping_pong).to(eq([1, 2, 3, 4, 5]))
+  end
+  it("replaces numbers divisible by 3 with ping, numbers diviisible by 5 with pong and numbers divisible by both with pingpong") do
+    expect((15).ping_pong).to(eq([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong"]))
+  end
+end
